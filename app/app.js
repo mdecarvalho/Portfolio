c22ed3177df0eb309740a02315db858fdbd7ba74
mdecarvalho <decarvalho.michael@gmail.com>
2015-09-23 11:17:37 +0200

    Modif - design of the home page

--- app/app.js
+++ app/app.js
@@ -6,22 +6,26 @@ var app = angular.module('mdcApp',['ngRoute'])
 
     $routeProvider.when('/', {
         templateUrl: 'app/views/home.html',
-        controller: 'homeController'
+        controller: 'mainController'
     })
 
     .when('/work', {
         templateUrl: 'app/views/work.html',
-        controller: 'workController',
+        controller: 'mainController',
     })
 
     .when('/aboutme', {
         templateUrl: 'app/views/aboutme.html',
-        controller: 'aboutMeController'
+        controller: 'mainController'
     })
 
     .when('/contactme', {
         templateUrl: 'app/views/contactme.html',
-        controller: 'contactMeController'
+        controller: 'mainController'
+    })
+    .when('/bonus', {
+        templateUrl: 'app/views/bonus.html',
+        controller: 'mainController'
     })
 
     .otherwise({
