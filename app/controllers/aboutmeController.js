8e0e1d44b918435ea505de317aeba47add62d7a7
mdecarvalho <decarvalho.michael@gmail.com>
2015-11-10 18:25:51 +0100

    Modif work page and add aboutme page

--- app/controllers/aboutmeController.js
+++ app/controllers/aboutmeController.js
@@ -3,44 +3,52 @@
 
 app
 .controller("aboutmeController", function($scope){
-    $scope.musique = [
+    $scope.songs = [
      {
-         title: "",
-         artiste: "",
-         image: "",
+         title: "LATCH",
+         artiste: "Disclosure & Sam Smith",
+         image: "images/songs/latch.jpg",
+         link:"https://www.youtube.com/watch?v=93ASUImTedo",
      },
      {
-         title: "",
-         artiste: "",
-         image: "",
+         title: "LONELY DAY",
+         artiste: "System of a Down",
+         image: "images/songs/lonelyday.jpg",
+         link:"https://www.youtube.com/watch?v=DnGdoEa1tPg",
      },
      {
-         title: "",
-         artiste: "",
-         image: "",
+         title: "WE CAN MAKE THE WORLD STOP",
+         artiste: "The Glitch Mob",
+         image: "images/songs/worldstop.png",
+         link:"https://www.youtube.com/watch?v=H-k_Eg7zXuc",
      },
      {
-         title: "",
-         artiste: "",
-         image: "",
+         title: "DEREZZED",
+         artiste: "Daft punk",
+         image: "images/songs/derezzed.jpg",
+         link:"https://www.youtube.com/watch?v=m4cgLL8JaVI",
      },
     ];
-    $scope.film = [
+    $scope.movies = [
      {
-         title: "",
-         image: "",
+         title: "THE PRESTIGE",
+         image: "images/movies/theprestige.jpg",
+         link:"https://www.youtube.com/watch?v=ijXruSzfGEc",
      },
      {
-         title: "",
-         image: "",
+         title: "GLADIATOR",
+         image: "images/movies/gladiator.jpg",
+         link:"https://www.youtube.com/watch?v=ol67qo3WhJk",
      },
      {
-         title: "",
-         image: "",
+         title: "PULP FICTION",
+         image: "images/movies/pulpfiction.jpg",
+         link:"https://www.youtube.com/watch?v=tGpTpVyI_OQ",
      },
      {
-         title: "",
-         image: "",
+         title: "INTERSTELLAR",
+         image: "images/movies/Interstellar.jpg",
+         link:"https://www.youtube.com/watch?v=LY19rHKAaAg",
      },
     ];
 })
--- app/controllers/workController.js
+++ app/controllers/workController.js
@@ -8,24 +8,24 @@ app
         name: "Pizza company web site",
         technologies: ["Php5", "Html4","CSS2", "JavaScript", "Jquery"],
         description: "It's my first web project, developed in binomial. The instructions were to realize a simplified management site for a pizzeria. We created a registration page , a page with the listing of produces, a promotion page, a page to personalize pizzas, a page for the product cart and a payment page.",
-        image: "images/pizza.jpg",
+        image: "images/work/pizza.jpg",
         },
         {
         name: "Dolead Campaign Manager",
         technologies: ["Python", "Html5", "CSS3", "JavaScript", "Jquery", "Jinja 2"],
         description: "During an internship in the Dolead company, I participated in the development of the Dolead Campaign Manager in the front-end team, using the Scrum methodology. The application allowed to manage and optimize AdWords and Bing campaigns.",
-        image: "images/dolead.jpg",
+        image: "images/work/dolead.jpg",
         },
         {
         name: "Project Chat",
         technologies: ["Php", "Html5","CSS3", "JavaScript", "Jquery", "Ajax"],
         description: "This project was realized in a group and we organized the work according to the scrum method. We also used Trello to organise the various tasks of the project. Furthermore I was the lead developer because I had more experience in Php procedural than my colleagues.",
-        image: "images/chat.jpg",
+        image: "images/work/chat.jpg",
         },{
         name: "Project Super Mario Bros ",
         technologies: ["Html5","CSS3", "JavaScript", "Jquery"],
         description: "It was a three days project, we had to allow the user to move Mario in three direction, forward, backward and upward.  But I continue to improve this mini game by bringing new improvements, such as colisions and many other things...",
-        image: "images/mario.jpg",
+        image: "images/work/mario.jpg",
         },
         {
         name: "Development of a custom module for Drupal 7",
